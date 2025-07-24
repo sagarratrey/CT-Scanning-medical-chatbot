245a8a4e5e9ca53b13ce083610a5dcc194022375
sagarratrey <sagar44ratrey@gmail.com>
2025-07-24 21:15:08 +0530

    folder added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI project",
+    version = "0.0.0",
+    author = "sagar ratrey",
+    author_email = "sagar44ratrey@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
